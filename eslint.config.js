f14d4b7f059c75b22ddd96927c25a931e8ed9b56
Jiaxin Peng <jiaxin.peng@outlook.com>
2025-10-27 00:34:39 +0000

    chore: remove old ESLint configuration and add new ESLint config file

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default tseslint.config(
+  {
+    ignores: ['build/**', 'jest.config.ts'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/indent': 'off',
+    },
+  },
+);
